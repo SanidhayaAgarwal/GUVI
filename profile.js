86e00e678bd19741c7929878388058ffc764d8e5
SanidhayaAgarwal <51049197+SanidhayaAgarwal@users.noreply.github.com>
2023-07-08 23:04:07 +0530

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#update").click(function(e){
+        e.preventDefault();
+        $.post("http://localhost:8000",
+        {
+          Email: $("#email").val(),
+          Age: $("#age").val(),
+          Contact: $("#contact").val(),
+          Gender: $("#gender").val()
+        },
+        );
+          alert('It has been updated');
+        });
+      });
+  
+
